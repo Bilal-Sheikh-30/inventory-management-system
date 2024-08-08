6f71fd6e88cf2078d42b846c6b463f703291c266
Bilal-Sheikh-30 <bilalsheikh3011@gmail.com>
2024-08-08 14:12:42 +0500

    configure settings

--- stockminder/stockminder/settings.py
+++ stockminder/stockminder/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ims',
         'USER': 'root',
-        'PASSWORD': 'Beta@3011',
+        'PASSWORD': password,
         'HOST': 'localhost',
         'PORT': '3306',
     }
